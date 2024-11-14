ccba5f3281b469016736decb8d86c7626b94bdb2
diegoacevedoa <zchapuz@hotmail.com>
2024-11-14 12:40:08 -0500

    Se muestran errores en el mensaje de la excepción

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -7,7 +7,7 @@ export const findAll = async (req, res) => {
     res.json(response);
   } catch (error) {
     res.status(500);
-    res.json({ message: "Error consultando la información." });
+    res.json({ message: "Error consultando la información: " + error });
   }
 };
 
@@ -18,6 +18,6 @@ export const findOne = async (req, res) => {
     res.json(response);
   } catch (error) {
     res.status(500);
-    res.json({ message: "Error consultando la información." });
+    res.json({ message: "Error consultando la información: " + error });
   }
 };
